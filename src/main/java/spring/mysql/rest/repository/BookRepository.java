006f8cf3a3eb9f751a934e6f6fda880dede85408
Manthan Bhatt <manthan@markettime.com>
2022-02-18 16:26:56 +0530

    Created spring rest api for mysql database

---
+++ src/main/java/spring/mysql/rest/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package spring.mysql.rest.repository;
+import spring.mysql.rest.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
